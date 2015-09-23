d77af24ad03ae4ac0f332b120019c9050b6cb1d4
Sophie POCHEZ <spochez@e3r8p6.42.fr>
2015-09-23 17:30:41 +0200

    .h

--- ft_ls.h
+++ ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: spochez <spochez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/20 15:40:23 by spochez           #+#    #+#             */
-/*   Updated: 2015/09/23 17:28:19 by spochez          ###   ########.fr       */
+/*   Updated: 2015/09/23 17:30:30 by spochez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,9 @@
 # include <unistd.h>
 # include <string.h>
 
+char	*ft_strdup(char *s1);
+char	**ft_realloc_tab(char **tab, int len);
+int		ft_strcmp(char *s1, char *s2);
+char	**ft_order(char **tab, int len);
 
 #endif
